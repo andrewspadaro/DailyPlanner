dded5ae40dfe17bb97b4d4e0e42929d8befcf7fe
Andrew Spadaro <andrewspadaro@gmail.com>
2020-11-06 16:08:21 -0500

    all times added up to 5pm

--- script.js
+++ script.js
@@ -9,3 +9,10 @@ saveBtn.on("click", function(){
 // load everything from local storage
 $("#9am .textBox").val(localStorage.getItem("9am"))
 $("#10am .textBox").val(localStorage.getItem("10am"))
+$("#11am .textBox").val(localStorage.getItem("11am"))
+$("#12pm .textBox").val(localStorage.getItem("12pm"))
+$("#1pm .textBox").val(localStorage.getItem("1pm"))
+$("#2pm .textBox").val(localStorage.getItem("2pm"))
+$("#3pm .textBox").val(localStorage.getItem("3pm"))
+$("#4pm .textBox").val(localStorage.getItem("4pm"))
+$("#5pm .textBox").val(localStorage.getItem("5pm"))
