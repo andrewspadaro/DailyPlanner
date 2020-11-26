0b7f43e0cb4abd62b2daa141589f01e6ac5b33a8
Andrew Spadaro <andrewspadaro@gmail.com>
2020-11-25 22:40:59 -0500

    was able to get the planner to display color depending on the time

--- script.js
+++ script.js
@@ -1,4 +1,9 @@
 var saveBtn = $(".saveBtn");
+var now = moment();
+var time = now.hour()
+//  + ':' + now.minutes() + ':' + now.seconds();
+time = time + ((now.hour()) >= 12 ? ' PM' : ' AM');
+console.log(time);
 
 saveBtn.on("click", function(){
     var textToSave = $(this).siblings().val()
@@ -16,3 +21,15 @@ $("#2pm .textBox").val(localStorage.getItem("2pm"))
 $("#3pm .textBox").val(localStorage.getItem("3pm"))
 $("#4pm .textBox").val(localStorage.getItem("4pm"))
 $("#5pm .textBox").val(localStorage.getItem("5pm"))
+
+var objDate = new Date();
+    var hours = objDate.getHours();
+    if(hours >= 9 && hours <= 17){
+        $(".hour").addClass("present");
+    }
+    else if(hours < 9){
+        $(".hour").addClass("past");
+    }
+    else{
+        $(".hour").addClass("future");
+    }
