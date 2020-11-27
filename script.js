b8e5acc7697cd9020861fedf7e8361eb3a794a73
Andrew Spadaro <andrewspadaro@gmail.com>
2020-11-27 16:53:31 -0500

    everything is working with using moment

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 var saveBtn = $(".saveBtn");
 var now = moment();
-var time = now.hour()
+var time = now.hours();
 //  + ':' + now.minutes() + ':' + now.seconds();
-time = time + ((now.hour()) >= 12 ? ' PM' : ' AM');
+time = time + ((now.hour()) >= 12 ? 'pm' : 'am');
 console.log(time);
 
 saveBtn.on("click", function(){
@@ -12,24 +12,37 @@ saveBtn.on("click", function(){
 })
 
 // load everything from local storage
-$("#9am .textBox").val(localStorage.getItem("9am"))
-$("#10am .textBox").val(localStorage.getItem("10am"))
-$("#11am .textBox").val(localStorage.getItem("11am"))
-$("#12pm .textBox").val(localStorage.getItem("12pm"))
-$("#1pm .textBox").val(localStorage.getItem("1pm"))
-$("#2pm .textBox").val(localStorage.getItem("2pm"))
-$("#3pm .textBox").val(localStorage.getItem("3pm"))
-$("#4pm .textBox").val(localStorage.getItem("4pm"))
-$("#5pm .textBox").val(localStorage.getItem("5pm"))
+$("#9 .textBox").val(localStorage.getItem("9"))
+$("#10 .textBox").val(localStorage.getItem("10"))
+$("#11 .textBox").val(localStorage.getItem("11"))
+$("#12 .textBox").val(localStorage.getItem("12"))
+$("#13 .textBox").val(localStorage.getItem("13"))
+$("#14 .textBox").val(localStorage.getItem("14"))
+$("#15 .textBox").val(localStorage.getItem("15"))
+$("#16 .textBox").val(localStorage.getItem("16"))
+$("17 .textBox").val(localStorage.getItem("17"))
 
-var objDate = new Date();
-    var hours = objDate.getHours();
-    if(hours >= 9 && hours <= 17){
-        $(".hour").addClass("present");
-    }
-    else if(hours < 9){
-        $(".hour").addClass("past");
-    }
-    else{
-        $(".hour").addClass("future");
-    }
+$('#' + time).children().addClass('present')
+var allHours = $('.hour').parent()
+console.log(allHours)
+allHours.each(function(key) {
+    var hourLoop = parseInt($(this).attr('id'))
+    var currentHour = parseInt(time.replace(/\D/g,''))
+    console.log(hourLoop , currentHour)
+  
+    
+    
+    if (hourLoop < currentHour) {
+        $(this).children().addClass("past");
+      } 
+      else if (hourLoop === currentHour) {
+        $(this).children().removeClass("past");
+        $(this).children().addClass("present");
+      } 
+      else {
+        $(this).children().removeClass("past");
+        $(this).children().removeClass("present");
+        $(this).children().addClass("future");
+      }
+    
+})  
